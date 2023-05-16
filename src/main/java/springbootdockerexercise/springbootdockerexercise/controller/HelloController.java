54de6d087dcaf9cc82e1a236e5c9d45ad72a553c
moonwonki <moonwonki1999@gmail.com>
2023-05-16 16:05:14 +0900

    feat : shellScript 준비

--- src/main/java/springbootdockerexercise/springbootdockerexercise/controller/HelloController.java
+++ src/main/java/springbootdockerexercise/springbootdockerexercise/controller/HelloController.java
@@ -1,12 +1,38 @@
 package springbootdockerexercise.springbootdockerexercise.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 public class HelloController {
     @RequestMapping("/hello")
     public String hello(){
         return "hello!";
+    }
+
+    @GetMapping("/run-script")
+    public String runScript() {
+        try {
+            // 실행할 스크립트 경로
+            String scriptPath = "webhook1.sh";
+
+            // 스크립트 실행
+            Process process = Runtime.getRuntime().exec(scriptPath);
+
+            // 스크립트 실행이 완료될 때까지 대기
+            int exitCode = process.waitFor();
+
+            if (exitCode == 0) {
+                return "Script executed successfully.";
+            } else {
+                return "Script execution failed.";
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return "An error occurred while executing the script.";
+        }
     }
 }
---
+++ webhook1.sh
@@ -0,0 +1,4 @@
+curl \
+-H "Content-Type: application/json" \
+-d "{\"username\": \"Mymy\", \"content\": \"hello\"}"  \
+https://discord.com/api/webhooks/1090549186040184913/_nVKm8TfNkMjjZDz2lgNqbVQBmciLcgii6trO7nXIEz7LAtMHVRBXkSVwoSd-nYRGxzU
