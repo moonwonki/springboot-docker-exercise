1a030cbe861a2995463f9d820bd96e0eb491e621
moonwonki <moonwonki1999@gmail.com>
2023-05-16 14:18:14 +0900

    feat : first commit

---
+++ src/main/java/springbootdockerexercise/springbootdockerexercise/SpringbootDockerExerciseApplication.java
@@ -0,0 +1,13 @@
+package springbootdockerexercise.springbootdockerexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springbootdockerexercise/springbootdockerexercise/SpringbootDockerExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootdockerexercise.springbootdockerexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
